eac38ef96b37697a32bb0d28cd39bdbc777efec1
zuzakula <zuzka.kula@gmail.com>
2023-08-15 22:29:54 +0200

    feat: employee service

---
+++ src/app/employee.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Employee} from "./employee";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient) { }
+
+  public getEmployees(): Observable<Employee> {
+    return this.http.get<Employee>(`${this.apiServerUrl}/employee`);
+  }
+
+  public getEmployee(id: number): Observable<Employee> {
+    return this.http.get<Employee>(`${this.apiServerUrl}/employee/${id}`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee`, employee);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+  public deleteEmployee(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${id}`);
+  }
+}
